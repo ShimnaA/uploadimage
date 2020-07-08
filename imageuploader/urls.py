feaaee4105e8d8b38430b7ffd879832d9d11a5c5
shimna <shimnaa.arangil@gmail.com>
2020-07-08 17:40:05 +0530

    upload image files

---
+++ imageuploader/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('upload/', views.image_upload_view),
+    path('', views.index),
+]
--- imageuploader/views.py
+++ imageuploader/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .forms import ImageForm
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+
+    return render(request, 'index1.html')
+
+def image_upload_view(request):
+    """Process images uploaded by users"""
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            img_obj = form.instance
+            return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
+    else:
+        form = ImageForm()
+    return render(request, "index.html", {'form': form})
+
